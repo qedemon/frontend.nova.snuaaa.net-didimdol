0a1fd5b97f5e8c32c9d0d53473fcabc38b4b8c2b
qedemon <qedemon@snu.ac.kr>
2024-02-25 19:20:39 +0000

    account

--- src/Pages/Register/DepositSubmitPage/index.js
+++ src/Pages/Register/DepositSubmitPage/index.js
@@ -16,8 +16,8 @@ function DepositSubmitPage({onSubmit, ...props}){
     const onSubmitHandler = useCallback(
         ()=>{
             const value = inputRef.current.value;
-            if(value.length===0){
-                setMessage("이름은 한글자 이상 입력해주세요.");
+            if(/신입 \d{2} .{1,}/.test(value)===0){
+                setMessage("이름은 신입 학번 이름 입니다.");
             }
             else{
                 if(typeof(onSubmit)==="function"){
@@ -38,7 +38,7 @@ function DepositSubmitPage({onSubmit, ...props}){
                 </p>
             </MessageBoxHeader>
             <MessageBoxBody>
-                <Input ref={inputRef} placeholder="김이름"/>
+                <Input ref={inputRef} placeholder="신입 24 김이름"/>
                 <label>{message??"\u00A0"}</label>
             </MessageBoxBody>
             <MessageBoxFooter>
