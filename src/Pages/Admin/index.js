cc53943dcfe926b23ece62ade901eaac0b1fa6dc
qedemon <qedemon@snu.ac.kr>
2024-02-28 07:46:14 +0000

    로그인

--- src/Pages/Admin/index.js
+++ src/Pages/Admin/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MembersInfo from "./MembersInfo";
+import {useContext as useAuth} from "../../Context/Auth";
+import { useNavigate } from "react-router-dom";
 
 function Admin(props){
-    
-    return (
-        <MembersInfo/>
+    const auth = useAuth();
+    const navigate = useNavigate();
+    useEffect(
+        ()=>{
+            if(!auth?.userInfo?.isStaff)
+            navigate("/StaffLogin");
+        }
     )
+    return auth?.userInfo?.isStaff?
+    (
+        <MembersInfo/>
+    ):
+    <></>
 }
 
 export default Admin;
